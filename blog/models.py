ec0ea1f1c8a898e71456b501cdfeb5008972b2eb
japhet Omorodion <japhet.omorodion@gmail.com>
2018-12-13 21:41:39 +0100

    changed timezone.now() to timezone.now within model.py

--- blog/models.py
+++ blog/models.py
@@ -9,12 +9,12 @@ class Post(models.Model):
     author = models.ForeignKey('auth.User',on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    create_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
 
     def publish(self):
-        self.published_date = timezone.now()
+        self.published_date = timezone.now
         self.save() 
 
 
@@ -34,7 +34,7 @@ class Comment(models.Model):
     post = models.ForeignKey('blog.Post', related_name='comments',on_delete=models.CASCADE)
     author = models.CharField(max_length=200)
     text = models.TextField()
-    create_date = models.DateTimeField(default=timezone.now())
+    create_date = models.DateTimeField(default=timezone.now)
     approved_comment = models.BooleanField(default=False)
 
     def approve(self):
